1083dfd6561fdb08f487ed5b9d93c8a230ef932d
TVR Raviteja <79498575+Tvrr28@users.noreply.github.com>
2021-08-01 21:55:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ Created on Sun August 1 10:30:00 2021
 import numpy as np
 import pickle
 import pandas as pd
-import streamlit as st
-import sklearn
+import streamlit as st 
 
 from PIL import Image
 
@@ -21,32 +20,31 @@ def welcome():
 
 def main():
     st.title("Amazon Inc. Stock Price Prediction")
+
+
+    
     html_temp = """
-    <div style="background-color:tomato;padding:10px">
+    <div style="background-color:#FFD700;padding:10px">
     <h2 style="color:white;text-align:center;">Amazon Close Price Predictor</h2>
     </div>
     """
-    page_bg_img = """
-    <style>
-    body {
-    background-image: url("https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80");
-    background-size: cover;
-    }
-    </style>
-    """
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+
     st.markdown(html_temp,unsafe_allow_html=True)
+
+
+
     Open = st.text_input("Open","Enter a value")
     High = st.text_input("High","Enter a value")
     Low = st.text_input("Low","Enter a value")
     Volume = st.text_input("Volume","Enter a value")
-    compound = st.text_input("compound","Enter a value based on recent headlines")
-    negative = st.text_input("negative","Enter a value based on recent headlines")
-    neutral = st.text_input("neutral","Enter a value")
-    positive = st.text_input("positive","Enter a value")
-    Subjectivity = st.text_input("Subjectivity","Enter a value")
-    Polarity = st.text_input("Polarity","Enter a value")
+    
+    
+    compound = st.slider("Compound",min_value=0.00 , max_value = 1.00 ,step = 0.01)
+    negative = st.slider("negative",min_value=0.00 , max_value = 1.00 ,step = 0.01)
+    neutral = st.slider("neutral",min_value=0.00 , max_value = 1.00 ,step = 0.01)
+    positive = st.slider("positive",min_value=0.00 , max_value = 1.00 ,step = 0.01)
+    Subjectivity = st.slider("Subjectivity",min_value=0.00 , max_value = 1.00 ,step = 0.01)
+    Polarity = st.slider("Polarity",min_value=0.00 , max_value = 1.00 ,step = 0.01)
 
     af = pd.DataFrame()
     af['compound'] = compound,compound
@@ -65,7 +63,7 @@ def main():
 
     if st.button("Predict"):
         result = linear.predict(af)[0]
-    st.success('Predicted Close Price : {}'.format(result))
+    st.success('Predicted Close Price : $ {}'.format(result))
     if st.button("About"):
         st.text("Close price of a stock is predicted based on news headlines and historical data using Linear Regression")
 
